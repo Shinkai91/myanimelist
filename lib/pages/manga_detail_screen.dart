085046351ff7966dc0fddfc4e189009f8e711d20
Shinkai91 <yosiaaser5@gmail.com>
2025-01-09 14:11:35 +0700

    manga detail fix unsed

--- lib/pages/manga_detail_screen.dart
+++ lib/pages/manga_detail_screen.dart
@@ -570,24 +570,3 @@ class _MangaDetailPageState extends State<MangaDetailScreen> {
     );
   }
 }
-
-class GenreChip extends StatelessWidget {
-  final String label;
-
-  const GenreChip({super.key, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      decoration: BoxDecoration(
-        color: Colors.blue.shade100,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Text(
-        label,
-        style: const TextStyle(color: Colors.blue),
-      ),
-    );
-  }
-}
